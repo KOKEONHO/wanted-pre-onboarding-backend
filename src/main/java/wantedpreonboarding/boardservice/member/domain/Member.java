ae7b09bc4a4ae9e70ce8f76d8ccdab879296cd94
GOMUNGNAM <rhrjsgh97@gmail.com>
2023-08-14 20:50:58 +0900

    Feat: articles 필드에 @OneToMany 어노테이션 추가

--- src/main/java/wantedpreonboarding/boardservice/member/domain/Member.java
+++ src/main/java/wantedpreonboarding/boardservice/member/domain/Member.java
@@ -1,10 +1,14 @@
 package wantedpreonboarding.boardservice.member.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import lombok.Getter;
@@ -29,4 +33,6 @@ public class Member {
 	@Column(name = "password")
 	private String password;
 
+	@OneToMany(mappedBy = "writer")
+	private List<Article> articles = new ArrayList<>();
 }
