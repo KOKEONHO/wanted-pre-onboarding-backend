c307670a0c548be6f6f6c344dc04c90b9b05e3f4
GOMUNGNAM <rhrjsgh97@gmail.com>
2023-08-15 02:30:27 +0900

    Feat: MemberService 생성

---
+++ src/main/java/wantedpreonboarding/boardservice/member/business/MemberService.java
@@ -0,0 +1,14 @@
+package wantedpreonboarding.boardservice.member.business;
+
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class MemberService {
+
+}
