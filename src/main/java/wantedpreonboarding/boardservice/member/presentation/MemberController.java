cebd15b6bab4de9f33806850c105b997f2afd5fe
GOMUNGNAM <rhrjsgh97@gmail.com>
2023-08-15 02:31:14 +0900

    Feat: MemberController 생성

---
+++ src/main/java/wantedpreonboarding/boardservice/member/presentation/MemberController.java
@@ -0,0 +1,11 @@
+package wantedpreonboarding.boardservice.member.presentation;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+public class MemberController {
+
+}
