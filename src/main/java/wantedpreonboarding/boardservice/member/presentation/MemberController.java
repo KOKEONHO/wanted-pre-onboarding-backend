7177c499da9263672685a40bfec95489ebe82f10
GOMUNGNAM <rhrjsgh97@gmail.com>
2023-08-15 02:37:17 +0900

    Feat: memberService 필드 선언 및 필요 어노테이션 추가

--- src/main/java/wantedpreonboarding/boardservice/member/presentation/MemberController.java
+++ src/main/java/wantedpreonboarding/boardservice/member/presentation/MemberController.java
@@ -1,13 +1,18 @@
 package wantedpreonboarding.boardservice.member.presentation;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import wantedpreonboarding.boardservice.member.business.MemberService;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
 public class MemberController {
+
+	private final MemberService memberService;
 
 }
