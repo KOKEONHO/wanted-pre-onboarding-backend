7d58495736be6746f4f607cfe2ae5a9405ca488c
GOMUNGNAM <rhrjsgh97@gmail.com>
2023-08-15 12:16:44 +0900

    Feat: MemberRepository에 사용자의 email로 사용자를 조회할 수 있는 기능 추가

--- src/main/java/wantedpreonboarding/boardservice/member/domain/MemberRepository.java
+++ src/main/java/wantedpreonboarding/boardservice/member/domain/MemberRepository.java
@@ -1,7 +1,11 @@
 package wantedpreonboarding.boardservice.member.domain;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
+	Optional<Member> findByEmail(@Param("email") String email);
 }
