a245599cbf86d16a1990b7b5200f58e9b3f1f96b
GOMUNGNAM <rhrjsgh97@gmail.com>
2023-08-15 20:08:46 +0900

    Feat: MemberService 내부 신규 사용자의 회원가입 기능 추가

--- src/main/java/wantedpreonboarding/boardservice/member/business/MemberService.java
+++ src/main/java/wantedpreonboarding/boardservice/member/business/MemberService.java
@@ -10,9 +10,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 import lombok.RequiredArgsConstructor;
 import wantedpreonboarding.boardservice.exception.RestApiException;
+import wantedpreonboarding.boardservice.member.domain.Member;
 import wantedpreonboarding.boardservice.member.domain.MemberRepository;
 import wantedpreonboarding.boardservice.member.presentation.dto.request.MemberRegisterRequest;
 import wantedpreonboarding.boardservice.member.presentation.dto.response.MemberRegisterResponse;
+import wantedpreonboarding.boardservice.util.PasswordEncoder;
 
 @Service
 @Transactional
@@ -20,15 +22,16 @@ import wantedpreonboarding.boardservice.member.presentation.dto.response.MemberR
 public class MemberService {
 
 	private final MemberRepository memberRepository;
+	private final PasswordEncoder passwordEncoder;
 
 	public MemberRegisterResponse register(@Valid @RequestBody MemberRegisterRequest request) {
 		memberRepository.findByEmail(request.getEmail())
 			.ifPresent(member -> {
-				throw new RestApiException(SAME_EMAIL_ALREADY_EXISTS)
+				throw new RestApiException(SAME_EMAIL_ALREADY_EXISTS);
 			}); // 중복 email로 가입 시도 시에 예외 발생
-
-
-
+		String encryptPassword = passwordEncoder.encrypt(request.getEmail(), request.getPassword());
+		Member saveMember = memberRepository.save(Member.of(request, encryptPassword));
+		return new MemberRegisterResponse(saveMember);
 	}
 
 }
