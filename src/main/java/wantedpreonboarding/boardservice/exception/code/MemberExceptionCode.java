25671105536f9956ed4abbaaf911d0ac82373365
GOMUNGNAM <rhrjsgh97@gmail.com>
2023-08-15 18:58:03 +0900

    Refactor: 회원가입 시 로그인 ID를 따로 생성하지 않으므로 함께 입력하는 email로 교체

--- src/main/java/wantedpreonboarding/boardservice/exception/code/MemberExceptionCode.java
+++ src/main/java/wantedpreonboarding/boardservice/exception/code/MemberExceptionCode.java
@@ -11,7 +11,7 @@ public enum MemberExceptionCode implements Code {
 	AUTHENTICATION_INVALID_USER(false, 401, -40104, "이용할 수 없는 사용자입니다."),
 	REQUIRED_REGISTER(false, 403, -40301, "회원가입이 필요한 사용자입니다."),
 	WRONG_PASSWORD(false, 401, -40101, "비밀번호가 틀렸습니다."),
-	SAME_ID_ALREADY_EXISTS(false, 400, -40000, "중복된 ID입니다.");
+	SAME_EMAIL_ALREADY_EXISTS(false, 400, -40000, "중복된 email입니다.");
 
 	private final boolean success;
 	private final int status;
