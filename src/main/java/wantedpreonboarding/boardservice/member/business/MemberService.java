2de8fb601d293f402e4655a24de13ec27941ba8f
GOMUNGNAM <rhrjsgh97@gmail.com>
2023-08-16 11:40:59 +0900

    Feat: 기존 회원가입을 한 사용자가 로그인 할 수 있도록 하는 기능 추가

--- src/main/java/wantedpreonboarding/boardservice/member/business/MemberService.java
+++ src/main/java/wantedpreonboarding/boardservice/member/business/MemberService.java
@@ -7,13 +7,19 @@ import javax.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import wantedpreonboarding.boardservice.exception.RestApiException;
+import wantedpreonboarding.boardservice.jwt.JwtTokenProvider;
 import wantedpreonboarding.boardservice.member.domain.Member;
 import wantedpreonboarding.boardservice.member.domain.MemberRepository;
+import wantedpreonboarding.boardservice.member.presentation.dto.MemberIdEmailDto;
+import wantedpreonboarding.boardservice.member.presentation.dto.request.MemberLoginRequest;
 import wantedpreonboarding.boardservice.member.presentation.dto.request.MemberRegisterRequest;
+import wantedpreonboarding.boardservice.member.presentation.dto.response.MemberLoginResponse;
 import wantedpreonboarding.boardservice.member.presentation.dto.response.MemberRegisterResponse;
 import wantedpreonboarding.boardservice.util.PasswordEncoder;
 
+@Slf4j
 @Service
 @Transactional
 @RequiredArgsConstructor
@@ -21,6 +27,7 @@ public class MemberService {
 
 	private final MemberRepository memberRepository;
 	private final PasswordEncoder passwordEncoder;
+	private final JwtTokenProvider jwtTokenProvider;
 
 	public MemberRegisterResponse register(MemberRegisterRequest request) {
 		memberRepository.findByEmail(request.getEmail())
@@ -32,4 +39,15 @@ public class MemberService {
 		return new MemberRegisterResponse(saveMember);
 	}
 
+	public MemberLoginResponse login(MemberLoginRequest request) {
+		log.info("[MemberService] [login]");
+		Member member = memberRepository.findByEmail(request.getEmail())
+			.orElseThrow(() -> new RestApiException(REQUIRED_REGISTER));
+		if (!member.getPassword().equals(passwordEncoder.encrypt(request.getEmail(), request.getPassword()))) {
+			throw new RestApiException(WRONG_PASSWORD);
+		}
+		String token = jwtTokenProvider.createToken(MemberIdEmailDto.from(member));
+		log.info("[MemberService] [token] {}", token);
+		return MemberLoginResponse.of(member.getId(), token);
+	}
 }
