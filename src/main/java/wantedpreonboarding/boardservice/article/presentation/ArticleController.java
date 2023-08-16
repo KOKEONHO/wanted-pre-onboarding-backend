25191da472adf90629355d94b12ac1e30aa05543
GOMUNGNAM <rhrjsgh97@gmail.com>
2023-08-16 14:48:38 +0900

    Feat: 전체 게시글 조회하는 기능에 Pageable 적용

--- src/main/java/wantedpreonboarding/boardservice/article/presentation/ArticleController.java
+++ src/main/java/wantedpreonboarding/boardservice/article/presentation/ArticleController.java
@@ -4,6 +4,11 @@ import static wantedpreonboarding.boardservice.response.code.success.SuccessResp
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.data.domain.Pageable;
+
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +19,7 @@ import lombok.extern.slf4j.Slf4j;
 import wantedpreonboarding.boardservice.article.business.ArticleService;
 import wantedpreonboarding.boardservice.article.presentation.dto.request.ArticleRequest;
 import wantedpreonboarding.boardservice.article.presentation.dto.response.ArticleIdResponse;
+import wantedpreonboarding.boardservice.article.presentation.dto.response.ArticlesResponse;
 import wantedpreonboarding.boardservice.response.dto.ResponseDto;
 
 @Slf4j
@@ -23,6 +29,13 @@ import wantedpreonboarding.boardservice.response.dto.ResponseDto;
 public class ArticleController {
 
 	private final ArticleService articleService;
+
+	@GetMapping
+	public ResponseDto<ArticlesResponse> showArticles(
+		@PageableDefault(size = 5, page = 0, sort = {"postedAt", "id"}, direction = Sort.Direction.DESC) Pageable pageable) {
+		ArticlesResponse articlesResponse = articleService.showArticles(pageable);
+		return ResponseDto.of(RESPONSE_SUCCESS, articlesResponse);
+	}
 
 	@PostMapping("/form")
 	public ResponseDto<ArticleIdResponse> createArticle(HttpServletRequest httpServletRequest,
