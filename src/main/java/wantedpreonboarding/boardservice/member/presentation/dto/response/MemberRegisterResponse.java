8c3208e247083885311d96cb5ce5bc3f51c855a3
GOMUNGNAM <rhrjsgh97@gmail.com>
2023-08-17 01:56:55 +0900

    Refactor: 기존 MemberRegisterResponse의 생성자로 Member 엔티티를 받던 부분을 id만 받도록 수정

--- src/main/java/wantedpreonboarding/boardservice/member/presentation/dto/response/MemberRegisterResponse.java
+++ src/main/java/wantedpreonboarding/boardservice/member/presentation/dto/response/MemberRegisterResponse.java
@@ -1,15 +1,14 @@
 package wantedpreonboarding.boardservice.member.presentation.dto.response;
 
 import lombok.Getter;
-import wantedpreonboarding.boardservice.member.domain.Member;
 
 @Getter
 public class MemberRegisterResponse {
 
 	private Long registeredMemberId;
 
-	public MemberRegisterResponse(Member member) {
-		this.registeredMemberId = member.getId();
+	public MemberRegisterResponse(Long memberId) {
+		this.registeredMemberId = memberId;
 	}
 
 }
