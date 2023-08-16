d0aa04011ba1ed5f6e40a7a0a75e4d03ada1b844
GOMUNGNAM <rhrjsgh97@gmail.com>
2023-08-16 13:44:42 +0900

    Feat: 사용자의 게시물 생성에 대한 응답에 쓰이는 ArticleIdResponse 생성

---
+++ src/main/java/wantedpreonboarding/boardservice/article/presentation/dto/response/ArticleIdResponse.java
@@ -0,0 +1,12 @@
+package wantedpreonboarding.boardservice.article.presentation.dto.response;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class ArticleIdResponse {
+
+	private final Long id;
+
+}
