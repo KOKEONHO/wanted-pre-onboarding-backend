86b0db5b9ba9d200c4fc7647cf70313ac0701bbe
GOMUNGNAM <rhrjsgh97@gmail.com>
2023-08-16 16:40:38 +0900

    Feat: 특정 게시글을 조회할 수 있도록 하는 기능 추가

--- src/main/java/wantedpreonboarding/boardservice/article/presentation/ArticleController.java
+++ src/main/java/wantedpreonboarding/boardservice/article/presentation/ArticleController.java
@@ -5,10 +5,10 @@ import static wantedpreonboarding.boardservice.response.code.success.SuccessResp
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.data.domain.Pageable;
-
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +18,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import wantedpreonboarding.boardservice.article.business.ArticleService;
 import wantedpreonboarding.boardservice.article.presentation.dto.request.ArticleRequest;
+import wantedpreonboarding.boardservice.article.presentation.dto.response.ArticleDetailResponse;
 import wantedpreonboarding.boardservice.article.presentation.dto.response.ArticleIdResponse;
 import wantedpreonboarding.boardservice.article.presentation.dto.response.ArticlesResponse;
 import wantedpreonboarding.boardservice.response.dto.ResponseDto;
@@ -32,9 +33,18 @@ public class ArticleController {
 
 	@GetMapping
 	public ResponseDto<ArticlesResponse> showArticles(
-		@PageableDefault(size = 5, page = 0, sort = {"postedAt", "id"}, direction = Sort.Direction.DESC) Pageable pageable) {
+		@PageableDefault(size = 5, page = 0, sort = {"postedAt",
+			"id"}, direction = Sort.Direction.DESC) Pageable pageable) {
 		ArticlesResponse articlesResponse = articleService.showArticles(pageable);
 		return ResponseDto.of(RESPONSE_SUCCESS, articlesResponse);
+	}
+
+	@GetMapping("/{articleId}")
+	public ResponseDto<ArticleDetailResponse> showArticle(HttpServletRequest httpServletRequest,
+		@PathVariable Long articleId) {
+		ArticleDetailResponse articleDetailResponse = articleService.findArticleDetailById(httpServletRequest,
+			articleId);
+		return ResponseDto.of(RESPONSE_SUCCESS, articleDetailResponse);
 	}
 
 	@PostMapping("/form")
